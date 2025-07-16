1dc5ba6cee5f76ab1f7632586dec612ec2c7f9fc
Aroa <aroa.rivas.2003@gmail.com>
2025-07-16 19:15:14 +0200

    cambios

--- FrontendAngular/sportify-me-frontend/src/app/auth/auth-module.ts
+++ FrontendAngular/sportify-me-frontend/src/app/auth/auth-module.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { Usuario } from '../../shared/models/usuario.model';
+import { Usuario } from '../shared/models';
 
 @Injectable({
   providedIn: 'root'
